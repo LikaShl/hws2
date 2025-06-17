6d78ad0318ea1de1c538ae0cc5819616c3bbf182
LikaShl <anzhelikashleiko@gmail.com>
2025-06-17 23:14:46 +0300

    make HW6

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -20,7 +20,7 @@ export const Layout: FC<PropsType> = ({ children }) => {
         <>
             <Sidebar open={open} handleClose={handleClose} />
             <Header handleOpen={handleOpen} />
-            <div>
+            <div className={'layout'}>
                 {/*страницы*/}
                 {children}
             </div>
